0fce990a5fe32b1c627c93131732e6ec3e661e3f
teleivo <teleivo@users.noreply.github.com>
2021-06-08 08:20:26 +0200

    WIP test rate limit lifted

--- ratelimit/token_test.go
+++ ratelimit/token_test.go
@@ -11,9 +11,6 @@ import (
 	"github.com/teleivo/go-distributed/ratelimit"
 )
 
-// TODO write a test for the returned rate limit headers for error case
-// TODO write a test that shows that sending requests while the rate limiter is
-// in the state of 429 will not prolong the wait necessary to get to 200
 // TODO speed up tests with smaller interval?
 
 func TestTokenBucket(t *testing.T) {
@@ -94,8 +91,8 @@ func TestTokenBucket(t *testing.T) {
 		if got := rsp.Header.Get("X-Ratelimit-Limit"); got != strconv.FormatUint(rate, 10) {
 			t.Errorf("Got %s, expected %d for header x-ratelimit-limit", got, rate)
 		}
-		if got := rsp.Header.Get("X-Ratelimit-Remaining"); got != strconv.FormatUint(rate-2, 10) {
-			t.Errorf("Got %s, expected %d for header x-ratelimit-remaining", got, rate-1)
+		if got := rsp.Header.Get("X-Ratelimit-Remaining"); got != "0" {
+			t.Errorf("Got %s, expected %d for header x-ratelimit-remaining", got, 0)
 		}
 		if got := rsp.Header.Get("X-Ratelimit-Used"); got != "2" {
 			t.Errorf("Got %s, expected %d for header x-ratelimit-used", got, 2)
@@ -121,14 +118,73 @@ func TestTokenBucket(t *testing.T) {
 		if got := rsp.Header.Get("X-Ratelimit-Used"); got != "2" {
 			t.Errorf("Got %s, expected %d for header x-ratelimit-used", got, 2)
 		}
-		// TODO test the timing
+		// TODO test the timing of ratelimit-reset
 		// x-ratelimit-reset: 1622955974
-		// TODO test that the first request will initiate the reset time
-		// reset := time.Now().Unix()
+		// goal: test that the first request will initiate the reset time to time.Now().Unix()
 		// test that the reset time stays the same for subsequent requests and
-		// only changes once the time has passed
+		// only changes once the time has passed the reset time
 		// if got := rsp.Header.Get("X-Ratelimit-Reset"); got != strconv.FormatInt(time.Now().Unix(), 10) {
 		// 	t.Errorf("Got %s, expected %d for header x-ratelimit-reset", got, 1)
+		// }
+	})
+	t.Run("TokensRefreshAfterResetTimePassed", func(t *testing.T) {
+		h := ratelimit.TokenBucket(1, time.Second, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+
+		// Exceed rate limit
+		h.ServeHTTP(httptest.NewRecorder(), nil)
+		rec := httptest.NewRecorder()
+		h.ServeHTTP(rec, nil)
+
+		rsp := rec.Result()
+		if got := rsp.StatusCode; got != 429 {
+			t.Errorf("Got %d, expected status 429", got)
+		}
+
+		rr, err := strconv.ParseInt(rsp.Header.Get("X-Ratelimit-Reset"), 10, 0)
+		if err != nil {
+			t.Fatalf("Failed to parse X-Ratelimit-Reset header, got %s", rsp.Header.Get("X-Ratelimit-Reset"))
+		}
+		reset := time.Unix(rr, 0)
+
+		// NOTE: converting X-Ratelimit-Reset into a time.Time gives a Time
+		// without a monotonic clock. So even if time.Now().After(reset) ==
+		// true in the test, it might not be in the actual rate limit handler.
+		// That is because inside the handler the monotonic clocks of the
+		// current time and the reset time will cause current.After(reset) to
+		// become true. Since we cannot tell what the monotonic clocks are
+		// inside of the actual implementation we can retry a few times within
+		// a few Milliseconds to make sure we pass the reset time.
+		time.Sleep(reset.Sub(time.Now()))
+
+		for {
+			select {
+			case <-time.Tick(time.Millisecond):
+				// Request allowed again
+				rec := httptest.NewRecorder()
+				h.ServeHTTP(rec, nil)
+
+				if rec.Result().StatusCode == 200 {
+					return
+				}
+			case <-time.After(time.Second):
+				t.Fatal("Timed out after 1s, waiting for rate limit to be lifted")
+			}
+		}
+
+		// for i := 0; i < 15; i++ {
+		// 	// Request allowed again
+		// 	rec = httptest.NewRecorder()
+		// 	h.ServeHTTP(rec, nil)
+
+		// 	rsp = rec.Result()
+		// 	if got := rsp.StatusCode; got == 200 {
+		// 		break
+		// 	}
+		// 	time.Sleep(50 * time.Millisecond)
+		// }
+
+		// if got := rsp.StatusCode; got != 200 {
+		// 	t.Errorf("Got %d, expected status 200", got)
 		// }
 	})
 	// TODO hard to read that I am making 11 requests but only expect 10 to
