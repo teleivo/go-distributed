4aafabc6196499da0abfed16ee31aa465e091024
teleivo <teleivo@users.noreply.github.com>
2021-06-08 08:48:15 +0200

    WIP rearrange tests

--- ratelimit/token_test.go
+++ ratelimit/token_test.go
@@ -20,37 +20,33 @@ import (
 
 func TestTokenBucket(t *testing.T) {
 	t.Run("AllowRequestsWithinLimit", func(t *testing.T) {
-		var rate uint64 = 10
-		interval := time.Second
 		var got uint64
-		h := ratelimit.TokenBucket(rate, interval, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		h := ratelimit.TokenBucket(1, time.Minute, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			atomic.AddUint64(&got, 1)
 		}))
 
-		// TODO can I rewrite this in a more readable form to highight that I
-		// want to send the requests within the given time frame? Maybe like in
-		// the error case
+		// Request 1 allowed
+		rec := httptest.NewRecorder()
+		h.ServeHTTP(rec, nil)
 
-		// Make rate number of calls at a rate of rate+1/interval
-		var calls uint64
-		s := time.Now()
-		var e time.Time
-		for range time.Tick(interval / time.Duration(rate+1)) {
-			h.ServeHTTP(httptest.NewRecorder(), nil)
-			calls++
-			if calls > rate-1 {
-				e = time.Now()
-				break
-			}
+		rsp := rec.Result()
+		if got := rsp.StatusCode; got != 200 {
+			t.Errorf("Got %d, expected status 200", got)
 		}
-		// Ensure the calls are made within the interval
-		// TODO is this necessary? Is it good enough to make rate number of
-		// calls within interval/(rate*1.2); adding 20% as buffer?
-		if d := e.Sub(s); d > interval {
-			t.Errorf("Took %v, expected calls to be made within %v", d, interval)
+		if got != 1 {
+			t.Errorf("Got %d, expected 1 call", got)
 		}
-		if got != uint64(rate) {
-			t.Errorf("Got %d, expected %d calls", got, rate)
+
+		// Request 2 denied
+		rec = httptest.NewRecorder()
+		h.ServeHTTP(rec, nil)
+
+		rsp = rec.Result()
+		if got := rsp.StatusCode; got != 429 {
+			t.Errorf("Got %d, expected status 429", got)
+		}
+		if got != 1 {
+			t.Errorf("Got %d, expected 1 call", got)
 		}
 	})
 	t.Run("RespondsWithRateLimitHeaders", func(t *testing.T) {
@@ -235,53 +231,6 @@ func TestTokenBucket(t *testing.T) {
 				}
 			case <-time.After(time.Second):
 				t.Fatal("Timed out after 1s, waiting for rate limit to be lifted")
-			}
-		}
-	})
-	// TODO hard to read that I am making 11 requests but only expect 10 to
-	// reach my wrapped handler
-	t.Run("DenyRequestsExceedingRateLimit", func(t *testing.T) {
-		var rate uint64 = 10
-		interval := time.Second
-		var got uint64
-		h := ratelimit.TokenBucket(rate, interval, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			atomic.AddUint64(&got, 1)
-		}))
-
-		// Make rate number of calls at a rate of rate+1/interval
-		var calls uint64
-		var e time.Time
-		start := time.Now()
-
-		for ; calls < rate; calls++ {
-			h.ServeHTTP(httptest.NewRecorder(), nil)
-		}
-		e = time.Now()
-		// Ensure the calls are made within the interval
-		// TODO is this necessary? Is it good enough to make rate number of
-		// calls within interval/(rate*1.2); adding 20% as buffer?
-		if d := e.Sub(start); d > interval {
-			t.Errorf("Took %v, expected calls to be made within %v", d, interval)
-		}
-		if got != uint64(rate) {
-			t.Errorf("Got %d, expected %d calls", got, rate)
-		}
-
-		w := httptest.NewRecorder()
-		h.ServeHTTP(w, nil)
-		if w.Result().StatusCode != 429 {
-			t.Errorf("Got %d, expected status 429", w.Result().StatusCode)
-		}
-
-		w = httptest.NewRecorder()
-		select {
-		case e := <-time.After(interval - time.Now().Sub(start)):
-			h.ServeHTTP(w, nil)
-			if d := e.Sub(start); d < interval {
-				t.Errorf("Waited %v before retrying, expected to wait until interval of %v elapsed", d, interval)
-			}
-			if w.Result().StatusCode != 200 {
-				t.Errorf("Got %d, expected status 200 after waiting for %v before retrying", w.Result().StatusCode, e.Sub(start))
 			}
 		}
 	})
